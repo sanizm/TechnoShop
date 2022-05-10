18f8eaa5bbaa94402a9e8343d7628c7687f6c13a
Saniz <sanizmomin07@gmail.com>
2022-05-10 14:36:41 -0400

    Components Files uploaded

---
+++ components/HeroBanner.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Link from "next/link";
+
+const HeroBanner = () => {
+  return (
+    <div className="hero-banner-container">
+      <div>
+        <p className="beats-solo">SMALL TEXT</p>
+        <h3>Mid TEXT</h3>
+        <img src="" alt="headphones" className="hero-banner-image" />
+        <div>
+          <Link href="/products/ID">
+            <button type="button">BuTTON TEXT</button>
+          </Link>
+          <div className="desc">
+            <h5>Description</h5>
+            <p>DESCRIPTION</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroBanner;
