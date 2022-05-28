4f1b1128da2b24f7441732eed693796ac1d3b953
Saniz <sanizmomin07@gmail.com>
2022-05-28 13:45:43 -0400

    components files added

--- components/HeroBanner.js
+++ components/HeroBanner.js
@@ -10,6 +10,7 @@ const HeroBanner = ({ heroBanner }) => {
         <p className="beats-solo">{heroBanner.smallText}</p>
         <h3>{heroBanner.midText}</h3>
         <h1>{heroBanner.largeText1}</h1>
+        
         <img
           src={urlFor(heroBanner.image)}
           alt="headphones"
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,19 @@
 import React from "react";
+import Link from 'next/link';
+import {AiOutlineShopping} from 'react-icons/ai'
+import Cart from '../components/Cart';
+import { useStateContext } from "../context/StateContext";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const {showCart, setShowCart, totalQuantities} = useStateContext();
+  return <div className="navbar-container"><p className='logo'>
+    <Link href= '/'>TechnoShop</Link></p>
+    <button type = 'button' className="cart-icon" onClick= {() => {setShowCart(prevVal => !prevVal)}}>
+      <AiOutlineShopping />
+      <span className="cart-item-qty">{totalQuantities}</span>
+    </button>
+ { showCart  &&   <Cart />}
+    </div>;
 };
 
 export default Navbar;
