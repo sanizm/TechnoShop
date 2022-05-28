d339ec700e3ff5f794da029a089696130d643b3f
Saniz <sanizmomin07@gmail.com>
2022-05-28 14:05:03 -0400

    bug fixed

--- components/Export.js
+++ components/Export.js
@@ -2,6 +2,6 @@ export { default as Footer } from "./Footer";
 export { default as Layout } from "./Layout";
 export { default as FooterBanner } from "./FooterBanner";
 export { default as HeroBanner } from "./HeroBanner";
-export { default as NavBar } from "./NavBar";
+export { default as NavBar } from "./Navbar";
 export { default as Product } from "./Product";
 export { default as Cart } from "./Cart";
